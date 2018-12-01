23f2e75cda1c577d05e34c9aa70d8e3fc5dfb7a4
Benjamin Congdon <bencongdon96@gmail.com>
2018-12-01 07:37:59 +0000

    Day1 in Python

---
+++ Day1-9/1.py
@@ -0,0 +1,20 @@
+import itertools
+
+def part_1(inputs):
+    return sum(int(i) for i in inputs)
+
+def part_2(inputs):
+    acc = 0
+    mem = {}
+    for i in itertools.cycle(int(j) for j in inputs):
+        acc += i
+        if acc in mem:
+            return acc
+        mem[acc] = True
+
+if __name__ == '__main__':
+    with open('1.txt') as f:
+        data = f.read().split()
+
+    print(part_1(data))
+    print(part_2(data))
